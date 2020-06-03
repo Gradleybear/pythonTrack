e7fee25490f933c7184de9322ea42fd15ba018d5
Gradleybear <pragyakulshresth@gmail.com>
2020-06-03 17:11:01 +0530

    small correction, misinterpret question

--- hackkerrank_prob2.py
+++ hackkerrank_prob2.py
@@ -8,13 +8,13 @@ import sys
 
 
 
-if __name__ == '__main__':
-    n = int(input().strip())
+
+n = 18
 if n%2==1:
   print("Weird")
 elif n%2==0 and n<=5 and n>=2:
  print("Not Weird")  #If n is even and in the inclusive range of 2  to 5, print Not
 elif n%2==0 and n>=6 and n<=20:
- print("Not Weird")
+ print("Weird")
 else:
  print("Not Weird")
